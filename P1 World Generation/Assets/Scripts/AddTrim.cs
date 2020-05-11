d8527695154aed852938a0091a390e822fb8a3a5
tausva <tautvydas.svarinskas@gmail.com>
2020-05-11 21:57:24 +0300

    P1 world generation finished

--- P1 World Generation/Assets/Scripts/AddTrim.cs
+++ P1 World Generation/Assets/Scripts/AddTrim.cs
@@ -9,10 +9,16 @@ public class AddTrim : MonoBehaviour
     public Sprite left;
     public Sprite top;
     public Sprite right;
+
     public Sprite bottomLeft;
     public Sprite bottomRight;
     public Sprite topLeft;
     public Sprite topRgiht;
+
+    public Sprite LTopLeft;
+    public Sprite LTopRight;
+    public Sprite LBottomLeft;
+    public Sprite LBottomRight;
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +57,41 @@ public class AddTrim : MonoBehaviour
                     {
                         tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = right;
                     }
+
+                    if (map[i, j + 1].Item2 != 4 && map[i + 1, j].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = LTopRight;
+                    }
+                    else if (map[i, j - 1].Item2 != 4 && map[i - 1, j].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = LBottomLeft;
+                    }
+                    else if (map[i, j - 1].Item2 != 4 && map[i + 1, j].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = LTopLeft;
+                    }
+                    else if (map[i, j + 1].Item2 != 4 && map[i - 1, j].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = LBottomRight;
+                    }
+
+                    if (map[i, j + 1].Item2 == 4 && map[i + 1, j].Item2 == 4 && map[i + 1, j + 1].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = bottomLeft;
+                    }
+                    else if (map[i, j - 1].Item2 == 4 && map[i - 1, j].Item2 == 4 && map[i - 1, j - 1].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = topRgiht;
+                    }
+                    else if (map[i, j + 1].Item2 == 4 && map[i - 1, j].Item2 == 4 && map[i - 1, j + 1].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = topLeft;
+                    }
+                    else if (map[i, j - 1].Item2 == 4 && map[i + 1, j].Item2 == 4 && map[i + 1, j - 1].Item2 != 4)
+                    {
+                        tiles[map[i, j].Item1].GetComponent<SpriteRenderer>().sprite = bottomRight;
+                    }
+
                 }
             }
         }
