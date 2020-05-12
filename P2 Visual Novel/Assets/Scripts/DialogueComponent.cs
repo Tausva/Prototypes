7416fb2cd0a98a20f1f92a8b7f980a4f36f2a912
tausva <tautvydas.svarinskas@gmail.com>
2020-05-12 21:53:11 +0300

    Added P2, as well as beginings of dialogue system

---
+++ P2 Visual Novel/Assets/Scripts/DialogueComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueComponent
+{
+    [SerializeField]
+    private string characterName;
+    
+    [SerializeField]
+    private List<string> dialogueLines = new List<string>();
+
+    public string GetLine()
+    {
+        if (dialogueLines.Count > 0)
+        {
+            string dialogueLine = dialogueLines[0];
+            dialogueLines.RemoveAt(0);
+            return dialogueLine;
+        }
+        return null;
+    }
+
+    public string GetCharacterName()
+    {
+        return characterName;
+    }
+}
